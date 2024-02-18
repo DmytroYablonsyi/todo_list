4d9ba8ae09aa46061646563a370c3184de30586d
Dmytro Yablonskiy <dmytroyablonskiy@MacBook-Air-Dmytro.local>
2024-02-18 14:42:06 +0200

    update list and complete button

--- src/component/item/index.js
+++ src/component/item/index.js
@@ -7,7 +7,7 @@ const Item = ( {id, title, done, makeTodoDone, deleteTodoItem} ) => {
 		<li>
 			<div className={`${styles.description} ${done ? styles.completed : ""}`}>{title}</div>
 			<div className={styles.buttons}>
-				<div onClick={() => makeTodoDone(id)}><img src= {done ? "https://cdn-icons-png.flaticon.com/128/4225/4225683.png" : 'https://cdn-icons-png.flaticon.com/128/665/665939.png'} alt='check'></img></div>
+				<div onClick={() => makeTodoDone(id)}><img src= {done ? "https://cdn-icons-png.flaticon.com/128/4225/4225683.png" : 'https://cdn-icons-png.flaticon.com/128/1442/1442912.png'} alt='check'></img></div>
 				<div onClick={() => deleteTodoItem(id)}><img src='https://cdn-icons-png.flaticon.com/128/399/399274.png' alt='cancel'></img></div>
 			</div>
 		</li>
